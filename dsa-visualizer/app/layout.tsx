8082b659250cb4d5f3e920f94e201fd5a38616a0
gael55x <gaille.amolong1@gmail.com>
2025-06-06 12:13:43 +0800

    Modified analytics

--- dsa-visualizer/app/layout.tsx
+++ dsa-visualizer/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "../lib/theme-context";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
